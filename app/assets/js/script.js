08c29fe6a1b8d32a7475a5a1c9a38b354eeb0e39
Paul <paulberthelot10@gmail.com>
2020-05-05 16:29:32 +0200

    ajax

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,10 +1,47 @@
 $( document ).ready(function() {
-    console.log('Ready to load the file');
-  // $.ajax({
-  // })
-  // .done(function( msg ) {
-  // })
-  // .fail(function( jqXHR, textStatus ) {
-  // });
-})  
+  console.log('Ready to load the file');
+  $.ajax({
+    type : "GET",
+    url :  "../docs/laposte_hexasmal.csv  ",
+    datatype : "text"
+  })
+    .done( function (data) {
+    // console.log(data)
+    $("#results").html(JSON.stringify(data))
+  })
+    .fail(function( jqXHR, textStatus,errorThrow ) {
+    //jqXHR sont les erreurs que le serveur à retourné.
+    console.log(jqXHR);
+    console.log(textStatus);
+    console.log(errorThrow);
+  })
 
+
+ 
+let tableau =[
+  //??? les données
+]
+ 
+ 
+ 
+//ceci est une erreur
+  /* const postal = [test];
+  const count = postal.push("test2");
+  console.log(postal);
+  console.log(count); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
